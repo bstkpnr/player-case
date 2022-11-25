69f38a29a7fc4dc05ca41783d8a5e050c99a8dc2
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-11-25 20:51:02 +0300

    edit app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.scss';
-import AddPlayer from './pages/AddPlayer';
+import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddPlayer from "./pages/AddPlayer";
+import ListPLayer from "./pages/ListPlayer";
 
 function App() {
   return (
     <div className="App">
-      <AddPlayer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<AddPlayer />} />
+          <Route path="/player-list" element={<ListPLayer />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
